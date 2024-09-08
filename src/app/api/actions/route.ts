11f99ee314f6df9619d0da6fca8bd3f7e41b03ae
Guney Uzel <devtex16@gmail.com>
2024-09-08 19:00:33 +0300

    rpc

--- src/app/api/actions/route.ts
+++ src/app/api/actions/route.ts
@@ -55,7 +55,7 @@ export const POST = async (req: Request) => {
 
     // Connect to the Solana network
     const connection = new Connection(
-      process.env.NEXT_PUBLIC_SOLANA_RPC_ENDPOINT ||
+      process.env.SOLANA_RPC_ENDPOINT ||
         "https://api.mainnet-beta.solana.com"
     );
 
